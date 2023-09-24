306e2b17c8708e786e4ca3a0985158f37fbe8e67
gemgupta <gemgupta007@gmail.com>
2023-09-24 11:52:05 +0530

    Onrefresh

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,29 +1,73 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../Store/CartContext";
 import Modal from "../../UI/Modal";
+import axios from "axios";
 import CartItem from "./CartItem";
 import "./Cart.css";
 function Cart(props) {
   const CartCtx = useContext(CartContext);
+  const [cartItems, setCartItems] = useState([]);
   const amount = CartCtx.totalAmount;
   const hasItem = CartCtx.item.length > 0;
-  const RemoveHandler = (id) => {
+  const RemoveHandler = async (id, _id) => {
     CartCtx.removeItem(id);
+    const existingItem = cartItems.find((item) => item._id === id);
+
+    if (existingItem) {
+      // Remove from local cart
+      const updatedCart = cartItems.filter((item) => item._id !== id);
+
+      CartCtx.setTotalAmount((totalAmount) => totalAmount - existingItem.price);
+      setCartItems(updatedCart);
+
+      // Remove from remote data
+      try {
+        await axios.delete(
+          `https://crudcrud.com/api/1c7b12e2bfde4ff2af994deac138c7cf/Med/${_id}`
+        );
+      } catch (error) {
+        console.error(
+          "Error deleting item from remote data:",
+          error.response.data
+        );
+      }
+    }
   };
+  useEffect(() => {
+    async function fetchCartItem() {
+      try {
+        const response = await axios.get(
+          `https://crudcrud.com/api/086320c71d22411c8d12f4204a1b2b33/Med`
+        );
+        const cart = response.data;
+        // const totalAmount = cart.reduce(
+        //   (total, item) => total + item.price,
+        //   0
+        // );
+        // CartCtx.setTotalAmount(totalAmount);
+
+        setCartItems(cart);
+        // CartCtx.getlength(cartItems.length);
+      } catch (error) {
+        console.error("Error fetching cart items:", error.response.data);
+      }
+    }
+    fetchCartItem();
+  }, []);
 
   const AddHandler = (item) => {
     CartCtx.addItem({ ...item, quantity: 1 });
   };
   const CartItems = (
     <ul className="cart-items">
-      {CartCtx.item.map((item) => {
+      {cartItems.map((item) => {
         return (
           <CartItem
             key={item.id}
             name={item.name}
             amount={item.quantity}
             price={item.price}
-            onRemove={RemoveHandler.bind(null, item.id)}
+            onRemove={RemoveHandler.bind(null, item.id, item._id)}
             onAdd={AddHandler.bind(null, item)}
           />
         );
--- src/Components/Meds/MedsForm.js
+++ src/Components/Meds/MedsForm.js
@@ -2,13 +2,30 @@ import React from "react";
 import { useContext } from "react";
 import "./MedsForm.css";
 import CartContext from "../Store/CartContext";
+import axios from "axios";
 function MedsForm(props) {
   const Cartctx = useContext(CartContext);
 
-  const AddItemHandler = (event) => {
+  const AddItemHandler = async (event) => {
     event.preventDefault();
-    console.log(props.item.quant);
-    Cartctx.addItem({ ...props.item, quantity: 1 });
+    // console.log(props.item.quant);
+    const itemToAdd= {
+      id: props.item.id,
+      name: props.item.name,
+      price: props.item.price,
+      quantity: 1
+    }
+    
+    Cartctx.addItem(itemToAdd);
+    try {
+      const response = await axios.post(
+        `https://crudcrud.com/api/086320c71d22411c8d12f4204a1b2b33/Med`,
+        itemToAdd
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
   let available = false;
   if (props.item.quant === 0) {
